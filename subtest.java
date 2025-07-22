726381be8d5fbdc153e92004f8d02cb0962c0505
unknown <skeigon0112@gmail.com>
2025-07-22 13:33:03 +0900

    サブのコミット

--- subtest.java
+++ subtest.java
@@ -1,1 +1,11 @@
-
+import java.util.*;
+import java.time.LocalTime;
+public class Main {
+    public static void main(String[] args) {
+    
+    	 System.out.println("これはサブテスト");
+    	 
+    LocalDateTime nowDate = LocalDateTime.now();
+ 		System.out.println(nowDate); 
+    }
+}
